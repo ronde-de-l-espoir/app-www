391c8a71c104c910b3b59a1eb7828d9ce402dff3
Silloky <ek.mindstorms@outlook.com>
2023-03-24 19:47:35 +0100

    Started work on Login page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <script src="./app.js"></script>
+    <title>Ajouter un paiement</title>
+</head>
+<body>
+    <main>
+        <div id="con-box">
+            <input type="password" placeholder="Mot de passe" required id="pwd">
+            <button id="btn" onclick="validate()">Connexion</button>
+        </div>
+    </main>
+</body>
+</html>
