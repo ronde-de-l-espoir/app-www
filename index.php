de774dd9b3001e8f1d875ebb6c2dfed2372e8360
Silloky <ek.mindstorms@outlook.com>
2023-03-25 07:41:40 +0100

    Feat : server-side pwd verification

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+
+    require('../db_config.php');
+    if (isset($_POST['submit'])) {
+        if ($_POST['pwd'] == $appPWd){
+            header('Location: ./main.php');
+        }
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,16 +17,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style.css">
-    <script src="./app.js"></script>
+    <!-- <script src="./app.js"></script> -->
     <title>Ajouter un paiement</title>
 </head>
 <body>
     <main>
         <h1>Connectez-vous</h1>
-        <div id="con-box">
-            <input type="password" placeholder="Mot de passe" required id="pwd">
-            <button id="btn" onclick="validate()">Connexion</button>
-        </div>
+        <form id="con-box" method="POST" action="index.php">
+            <input type="password" placeholder="Mot de passe" required id="pwd" name="pwd">
+            <button type="submit" id="btn" name="submit">Connexion</button>
+        </form>
     </main>
 </body>
 </html>
