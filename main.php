08b87b620af712d74a1c8d5414d31eccb7efb993
Silloky <ek.mindstorms@outlook.com>
2023-03-25 12:58:21 +0100

    Feat : DB insertion

--- main.php
+++ main.php
@@ -7,6 +7,36 @@
         header('Location: ./login.php');
     }
     require('../db_config.php');
+
+    if (isset($_POST)){
+        if (isset($_POST['annuler'])){
+            header('Location: ./login.php');
+        } elseif (isset($_POST['valider'])) {
+            $isAnonymous = true;
+            if ($_POST['type'] == 'don'){
+                $isDonSimple = true;
+                $isVente = false;
+            } elseif ($_POST['type'] == 'vente'){
+                $isVente = true;
+                $isDonSimple = false;;
+            }
+            if ($_POST['moyen'] == 'especes'){
+                $isCash = true;
+                $isCheque = false;
+            } elseif ($_POST['moyen'] == 'cheque'){
+                $isCash = false;
+                $isCheque = true;
+            }
+            $amount_donated = $_POST['montant'];
+
+            $sql = "INSERT INTO donations(amount_donated, isAnonymous, isCash, isCheque, isDonSimple, isVente) VALUES ('$amount_donated', '$isAnonymous', '$isCash', '$isCheque', '$isDonSimple', '$isVente')";
+            if (!mysqli_query($conn, $sql)) {
+                echo "Query error: " .mysqli_error($conn);
+            } else {
+                $dbSuccess = true;
+            }
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -20,7 +50,7 @@
 </head>
 <body>
     <h1>Veuillez compléter le formulaire</h1>
-    <form>
+    <form action="./main.php" method="post">
         <div class="form-element">
             <label for="type" class="main-label">Type de rentrée de fonds</label>
             <label><input type="radio" name="type" value="don">Don</label>
@@ -28,8 +58,8 @@
         </div>
         <div class="form-element">
             <label for="moyen" class="main-label">Moyen de paiement</label>
-            <label><input type="radio" name="moyen" value="don">Espèces</label>
-            <label><input type="radio" name="moyen" value="vente">Chèque</label>
+            <label><input type="radio" name="moyen" value="especes">Espèces</label>
+            <label><input type="radio" name="moyen" value="cheque">Chèque</label>
         </div>
         <div class="form-element" style="margin-bottom: 0;">
             <label for="montant" class="main-label">Montant</label>
