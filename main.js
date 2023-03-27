11593191529dd8f0aaefd0e504968bff802dba41
Silloky <ek.mindstorms@outlook.com>
2023-03-27 12:44:07 +0200

    Fix : valider disabled if montant unequal

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const firstMontantInput = document.getElementById('montant-1')
 const secondMontantInput = document.getElementById('montant-2')
 const errorText = document.getElementById('error-text')
+const validerBTN = document.getElementById('valider')
 
 function checkSame() {
     let montant1 = firstMontantInput.value
     let montant2 = secondMontantInput.value
     if (montant1 != montant2){
         errorText.classList.remove('hidden')
+        validerBTN.setAttribute('disabled')
     } else {
         errorText.classList.add('hidden')
     }
