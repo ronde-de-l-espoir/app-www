a99dd5880b9c81f473054cc10f9ce583a059def3
Elias Kirkwood <yannis3736@gmail.com>
2023-04-25 11:03:48 +0200

    Error Page on handle scan

--- gala-scanner/scan-flow/handle-scan.php
+++ gala-scanner/scan-flow/handle-scan.php
@@ -3,3 +3,23 @@
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="../../root.css">
+    <title>Scan QR Code</title>
+</head>
+<body>
+    <h3><span class="emote">:(</span>There seems to be a page missing. Please use another method while we take care of this problem.</h3>
+    
+    <div class="button-wrapper">
+        <button class="return" onclick="window.location='../how-to.php'">Retour</button>
+    </div>
+
+</body>
+</html>
