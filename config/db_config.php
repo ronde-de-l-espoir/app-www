3ec62013da86f856c74025e56d0041ea1fc5e395
Elias Kirkwood <yannis3736@gmail.com>
2023-04-25 13:11:04 +0200

    Update DB config according with remote modifs

--- config/db_config.php
+++ config/db_config.php
@@ -3,7 +3,7 @@
     $servername = "ronde-de-l-espoir.fr";
     $username = "ctzs1179";
     $password = "Delta43theta!";
-    $dbname = "ctzs1179_gala";
+    $dbname = "ctzs1179_lrde";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
