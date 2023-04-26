e4af42278bcdbc101f6ab2b95e3faaef38435f17
Elias Kirkwood <yannis3736@gmail.com>
2023-04-26 20:34:45 +0200

    Login Page

--- login/login.php
+++ login/login.php
@@ -22,8 +22,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./login.css">
-    <link rel="stylesheet" href="../common.css">
+    <link rel="stylesheet" href="../root.css">
     <!-- <script src="./app.js"></script> -->
-    <title>Ajouter un paiement</title>
+    <title>Login</title>
 </head>
 <body>
+
+    <main>
+        <div class="top-icon">
+            <img src="../img/lrde-icon.png" alt="icon">
+        </div>
+
+        <form action="login.php" method="POST">
+            <div class="field">
+                <img src="../img/account-icon.png" alt="icon">
+                <input type="text" autocomplete="off">
+            </div>
+
+            <div class="field">
+                <img src="../img/password-icon.png" alt="icon">
+                <input type="password" autocomplete="off">
+            </div>
+        </form>
+        <div class="trouble-manager">
+            <p><i>Identifiants oubliés ?</i></p>
+        </div>
+        
+    </main>
+
+</body>
+</html>
