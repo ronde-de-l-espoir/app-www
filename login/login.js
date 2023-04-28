9ed7ec36aa3c50c86a1cea2ef8c55895923c45f2
Skyman-2 <yannis3736@gmail.com>
2023-04-29 00:51:07 +0200

    Dis a pear. *poof* Disappeeaaar

--- login/login.js
+++ login/login.js
@@ -1,3 +1,13 @@
 document.querySelectorAll('#close-popup')[0].addEventListener('click', () => {
-    document.getElementById('error-popup').style.display = 'none';
+    disappear();
 });
+
+setTimeout(disappear, 3000)
+
+function disappear() {
+    const popup = document.querySelectorAll('#error-popup')[0];
+    popup.style.transform = 'translateY(-80px)';
+    setTimeout(() => {
+        popup.style.opacity = '0';
+    }, 100)
+}
