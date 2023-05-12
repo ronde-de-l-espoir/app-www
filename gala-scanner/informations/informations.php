43ad7267a52007ced22fbae131de7ff7d2ce98f7
Elias Kirkwood <yannis3736@gmail.com>
2023-05-12 23:07:42 +0200

    Add 5 main informations on individu card

--- gala-scanner/informations/informations.php
+++ gala-scanner/informations/informations.php
@@ -1,6 +1,15 @@
 <?php
 
+    require('../../../db_config.php');
 
+    if (isset($_GET['id'])) {
+        $id = mysqli_real_escape_string($conn, $_GET['id']);
+        $sql = "SELECT * FROM preinscriptions WHERE id = '$id'";
+        $res = mysqli_query($conn, $sql);
+        $ans = mysqli_fetch_assoc($res);
+    } else {
+        header('Location: ../how-to.php');
+    }
 
 ?>
 
@@ -10,11 +19,75 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../root.css">
     <link rel="stylesheet" href="./informations.css">
-    <link rel="stylesheet" href="../../common.css">
     <title>Informations</title>
 </head>
 <body>
+
+    <header>
+        <h1>Fiche d'individu</h1>
+    </header>
+
+    <?php if ($ans == null) : ?>
+        
+        <section class="not-found">
+            <img src="../../img/not-found-icon.png" alt="Not Found icon">
+            <p class="warning">Attention, ce ticket n'existe pas.</p>
+            <div class="button-wrapper">
+                <a href="../how-to.php" class="btn">Retour</a>
+            </div>
+        </section>
+        
+    <?php else : ?>
+
+        <main class="fiche">
+            <div class="main-wrapper">        
+
+                <section id="identity">
+                    <div class="code-display">
+                        <p><?php 
+                            for($i = 0; $i < 5; $i++) {
+                                echo $ans['id'][$i];
+                            }
+                        ?></p>
+                        <p><?php 
+                            for($i = 5; $i < 9; $i++) {
+                                echo $ans['id'][$i];
+                            }
+                        ?></p>
+                    </div>
+                    <div class="more-infos">
+                        <div id="id-display">
+                            <p><span id="fname"><?php echo $ans['fname'] ?></span>
+                            <span id="lname"><?php echo $ans['lname'] ?></span></p>
+                        </div>
+                        <div class="contact-infos">
+                            <p><span id="email"><?php echo $ans['email'] ?></span></p>
+                        </div>
+                    </div>
+                </section>
+
+                <section id="event">
+                    <div class="info-field">
+                        <?php if ($ans['event'] == 'Gala') : ?>
+                            <img src="../../img/show-icon.png" class="info-icon" alt="Event Icon">
+                        <?php elseif ($ans['event'] == 'Concert') : ?>
+                            <img src="../../img/concert-icon.png" class="info-icon" alt="Event Icon">
+                        <?php endif; ?>
+                        <p>Participe au <span class="important-info"><?php echo $ans['event'] ?>.</span></p>
+                    </div>
+                    <div class="info-field">
+                        <img src="../../img/crowd-icon.png" class="info-icon" alt="Icon">
+                        <p>Est accompagné par <span class="important-info"><?php echo $ans['nChildren'] ?> personnes.</span></p>
+                    </div>
+                </section>
+
+            </div>
+        </main>
+        
+    <?php endif ?>
     
-</body>
+    
+    </body>
 </html>
