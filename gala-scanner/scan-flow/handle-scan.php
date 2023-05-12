092b1c32f51e572619749b047d51da6056a72269
Elias Kirkwood <yannis3736@gmail.com>
2023-05-12 17:04:23 +0200

    Working on scanner

--- gala-scanner/scan-flow/handle-scan.php
+++ gala-scanner/scan-flow/handle-scan.php
@@ -1,13 +1,7 @@
-<?php
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style.css">
@@ -15,11 +9,32 @@
     <title>Scan QR Code</title>
 </head>
 <body>
-    <h3><span class="emote">:(</span>There seems to be a page missing. Please use another method while we take care of this problem.</h3>
-    
-    <div class="button-wrapper">
-        <button class="return" onclick="window.location='../how-to.php'">Retour</button>
-    </div>
+
+    <div id="reader" width="600px"></div>
+
+    <div id="result"></div>
+
+    <!-- <script src="./html5-qrcode.min.js" type="text/javascript"></script> -->
+    <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
+    <script>
+        function onScanSuccess(decodedText, decodedResult) {
+            // handle the scanned code as you like, for example:
+            console.log(`Code matched = ${decodedText}`, decodedResult);
+            document.getElementById('result').innerText = decodedText;
+        }
+
+        function onScanFailure(error) {
+            // handle scan failure, usually better to ignore and keep scanning.
+            // for example:
+            console.warn(`Code scan error = ${error}`);
+        }
+
+        let html5QrcodeScanner = new Html5QrcodeScanner(
+            "reader",
+            { fps: 10, qrbox: {width: 250, height: 250} },
+            /* verbose= */ false);
+        html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+    </script>
 
 </body>
 </html>
