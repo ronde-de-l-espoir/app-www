e3f7665c950a023c898ee6206febc885c25a9871
Elias Kirkwood <yannis3736@gmail.com>
2023-05-18 13:08:29 +0200

    Typo in pwd again -_-

--- config/db_config.php
+++ config/db_config.php
@@ -2,7 +2,7 @@
 
     $servername = "ronde-de-l-espoir.fr";
     $username = "ctzs1179";
-    $password = "Omega56alpha!";
+    $password = "Omega56alpha&";
     $dbname = "ctzs1179_lrde";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
