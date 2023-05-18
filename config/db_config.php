ea6d2d79db6f05583ae57771d4c0c569d548b911
Elias Kirkwood <yannis3736@gmail.com>
2023-05-18 13:00:08 +0200

    Update db_config

--- config/db_config.php
+++ config/db_config.php
@@ -2,7 +2,7 @@
 
     $servername = "ronde-de-l-espoir.fr";
     $username = "ctzs1179";
-    $password = "Delta43theta!";
+    $password = "Omega56theta!";
     $dbname = "ctzs1179_lrde";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
