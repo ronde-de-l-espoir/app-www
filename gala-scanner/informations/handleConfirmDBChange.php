0370948c921ba9c29fa4c281a6503a90c5879092
Elias Kirkwood <yannis3736@gmail.com>
2023-05-19 16:39:00 +0200

    Prepare authorAction save

--- gala-scanner/informations/handleConfirmDBChange.php
+++ gala-scanner/informations/handleConfirmDBChange.php
@@ -1,14 +1,7 @@
 <?php
-
-    // Trois cas différents existent :
-    //  - hasPaid = 0 && hasPaidAll = 0
-    //  => Cela veut dire que l'accompagnant n'a pas payé de quelques manières
-    //  - hasPaid = 1 && hasPaidAll = 0
-    //  => Cela veut dire que l'accompagnant s'est seulement payé sa place, laissant les autres payer eux-mêmes
-    //  - hasPaid = 1 && hasPaidAll = 1
-    //  => Cela signifie que l'accompagnant a payé pour tout le monde
-    //
-    //  Note : Le cas "hasPaid = 0 && hasPaidAll = 1" est impossible car l'accompagnant paie pour lui si il paie aussi pour les autres
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
 
     require('../../../db_config.php');
 
@@ -17,7 +10,8 @@
         $ids = json_decode($_GET['ids']);
         for ($i = 0; $i < count($ids); $i++) {
             $id = $ids[$i];
-            $sql = "UPDATE preinscriptions SET hasPaid = 1 WHERE id = $id";
+            $author = 'Confirmed Payment: ' . $_SESSION['identifiant'];
+            $sql = "UPDATE preinscriptions SET hasPaid = 1, actionAuthor = $author WHERE id = $id";
             if (!mysqli_query($conn, $sql)) {
                 $ans = [
                     "status" => 500,
@@ -37,7 +31,8 @@
 
     if (isset($_GET['id'])) {
         $id = $_GET['id'];
-        $sql = "UPDATE preinscriptions SET hasPaid = 1 WHERE id = $id";
+        $author = 'Confirmed Payment: ' . $_SESSION['identifiant'];
+        $sql = "UPDATE preinscriptions SET hasPaid = 1, actionAuthor = $author WHERE id = $id";
         if (!mysqli_query($conn, $sql)) {
             $ans = [
                 "status" => 500,
