7f6db19e19dd2ac19b9bbc460e2648529fb60815
Elias Kirkwood <yannis3736@gmail.com>
2023-05-19 15:55:12 +0200

    Prevent default behavior on alone flow form

--- gala-scanner/informations/information-alone.js
+++ gala-scanner/informations/information-alone.js
@@ -1,1 +1,10 @@
 console.log("-- Alone --")
+
+document.getElementById('form-payment').addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    const formData = new FormData(event.target);
+    const formObject = Object.fromEntries(formData.entries());
+    const totalPrice = formObject.totalPrice;
+    console.log(totalPrice);
+})
