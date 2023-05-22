4a78deefd0e869546203d972fbd6a54128668233
Elias Kirkwood <yannis3736@gmail.com>
2023-05-22 22:00:57 +0200

    Mostly done

--- money-form/app.js
+++ money-form/app.js
@@ -1,1 +1,10 @@
 console.log("Heartwarming welcome.");
+
+const errorMsg = document.getElementById('error-msg-wrapper');
+errorMsg.querySelectorAll('img')[0].addEventListener('click', () => {
+    errorMsg.style.opacity = '0.2';
+    errorMsg.style.transform = 'translateY(-120px)';
+    setTimeout(() => {
+        errorMsg.style.display = 'none'
+    }, 500)
+})
--- money-form/form.php
+++ money-form/form.php
@@ -1,7 +1,65 @@
 <?php
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    require('../../db_config.php');
+
+    function createErrorMessage($errorText) {
+        echo "
+            <div id='error-msg-wrapper'>
+                <div id='error-msg'>
+                    <img src='../img/close-icon.png' alt='close'>
+                    <h3>Erreur :</h3>
+                    <p>$errorText</p>
+                </div> 
+            </div>
+        ";
+    }
 
     if (isset($_POST['submit'])) {
-        echo $_POST['type'] . '<br>' . $_POST['moyen'];
+        $amount = $_POST['amount'];
+        $amountConfirm = $_POST['amountConfirm'];
+        if ($_POST['amount'] != '' && $_POST['amountConfirm'] != '') {
+    
+            if (!isset($_POST['type'])) {
+                createErrorMessage("Veuillez sélectionner le type de l'entrée.");
+            } else if (!isset($_POST['moyen'])) {
+                createErrorMessage("Veuillez sélectionner le moyen de paiement.");
+            } else if ($amount != $amountConfirm) {
+                createErrorMessage("Le montant diffère d'une case à l'autre.");
+            } else {
+                
+                $actionAuthor = $_SESSION['identifiant'];
+
+                if ($_POST['type'] == 'don'){
+                    $isDonSimple = true;
+                    $isVente = false;
+                } else if ($_POST['type'] == 'vente'){
+                    $isVente = true;
+                    $isDonSimple = false;;
+                }
+                if ($_POST['moyen'] == 'cash'){
+                    $isCash = true;
+                    $isCheque = false;
+                } else if ($_POST['moyen'] == 'cheque'){
+                    $isCash = false;
+                    $isCheque = true;
+                }
+
+                $sql = "INSERT INTO donations(real_amount, isCash, isCheque, isDonSimple, isVente, actionAuthor) VALUES ('$amount','$isCash','$isCheque','$isDonSimple','$isVente', '$actionAuthor')";
+
+                if (!mysqli_query($conn, $sql)) {
+                    echo 'Error in MySQL:' . mysqli_error($conn);
+                } else {
+                    // header('Location: ./success.php');
+                    echo "CONGRATS";
+                }
+
+            }
+        } else {
+            createErrorMessage("Le montant ou la confirmation du montant n'ont pas été rempli.");
+        }
     }
 
 ?>
@@ -12,8 +70,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
     <link rel="stylesheet" href="../root.css">
+    <script src="./app.js" defer></script>
     <title>Entéres d'argent</title>
 </head>
 <body>
@@ -27,16 +86,16 @@
             <div class="form-element">
                 <h3>Type de l'entrée :</h3>
                 <div class="input-wrapper">
-                    <label><input type="radio" name="type" value="don" required><div class="label-img" id="don"></div>Don</label>
-                    <label><input type="radio" name="type" value="vente" required><div class="label-img" id="vente"></div>Vente</label>
+                    <label><input type="radio" name="type" value="don"><div class="label-img" id="don"></div>Don</label>
+                    <label><input type="radio" name="type" value="vente"><div class="label-img" id="vente"></div>Vente</label>
                 </div>
             </div>
 
             <div class="form-element">
                 <h3>Moyen de paiement :</h3>
                 <div class="input-wrapper">
-                    <label><input type="radio" name="moyen" value="cash" required><div class="label-img" id="cash"></div>Espèces</label>
-                    <label><input type="radio" name="moyen" value="cheque" required><div class="label-img" id="cheque"></div>Chèque</label>
+                    <label><input type="radio" name="moyen" value="cash"><div class="label-img" id="cash"></div>Espèces</label>
+                    <label><input type="radio" name="moyen" value="cheque"><div class="label-img" id="cheque"></div>Chèque</label>
                 </div>
             </div>
 
@@ -48,7 +107,7 @@
                             type="number"
                             class="amount-input"
                             name="amount"
-                            value=""
+                            value="<?php if (isset($amount)) echo $amount ?>"
                             min="0"
                             max="10000"
                         >€
@@ -58,8 +117,8 @@
                         <input
                             type="number"
                             class="amount-input"
-                            name="amount"
-                            value=""
+                            name="amountConfirm"
+                            value="<?php if (isset($amountConfirm)) echo $amountConfirm ?>"
                             min="0"
                             max="10000"
                         >€
@@ -67,7 +126,11 @@
                 </div>
             </div>
 
-            <input type="submit" name="submit" value="submit">
+            <div class="button-field">
+                <input type="reset" name="reset" value="Effacer tout">
+                <input type="submit" name="submit" value="Valider">
+            </div>
+
         </form>
     </main>
     
