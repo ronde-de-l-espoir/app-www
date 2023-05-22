05fec52a0c3da489abc2d47472bf1baacf9ce3a8
Elias Kirkwood <yannis3736@gmail.com>
2023-05-22 18:48:43 +0200

    Work on inputs for form

--- money-form/form.php
+++ money-form/form.php
@@ -1,3 +1,11 @@
+<?php
+
+    if (isset($_POST['submit'])) {
+        echo $_POST['type'] . '<br>' . $_POST['moyen'];
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,9 +14,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="../root.css">
-    <title>Document</title>
+    <title>Entéres d'argent</title>
 </head>
 <body>
+
+    <header>
+        <h1>Entrées d'argent</h1>
+    </header>
+
+    <main>
+        <form action="./form.php" method="POST">
+            <div class="form-element">
+                <h3>Type de l'entrée :</h3>
+                <div class="input-wrapper">
+                    <label><input type="radio" name="type" value="don" required><img src="../img/don-icon.png" alt="" class="label-img">Don</label>
+                    <label><input type="radio" name="type" value="vente" required><img src="../img/vente-icon.png" alt="" class="label-img">Vente</label>
+                </div>
+            </div>
+
+            <div class="form-element">
+                <h3>Moyen de paiement :</h3>
+                <div class="input-wrapper">
+                    <label><input type="radio" name="moyen" value="cash" required><img src="../img/cash-icon.png" alt="" class="label-img">Espèces</label>
+                    <label><input type="radio" name="moyen" value="cheque" required><img src="../img/cheque-icon.png" alt="" class="label-img">Chèque</label>
+                </div>
+            </div>
+
+            <input type="submit" name="submit" value="submit">
+        </form>
+    </main>
     
 </body>
 </html>
