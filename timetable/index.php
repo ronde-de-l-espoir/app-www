9b6af39030b2f1636cd2d38cdb9afb2a12cb3307
Skyman-2 <yannis3736@gmail.com>
2023-05-25 13:39:47 +0200

    Prepare timeline page

---
+++ timetable/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="../root.css">
+    <title>Timeline</title>
+</head>
+<body>
+
+    <header>
+        <h1>Emploi du temps</h1>
+    </header>
+
+    <main>
+        <div class="main-wrapper">
+            <div class="period">
+                <img src="" alt="" class="period-image">
+                <h3 class="period-title"></h3>
+                <p class="period-text"></p>
+            </div>
+
+            <div class="period">
+                <img src="" alt="" class="period-image">
+                <h3 class="period-title"></h3>
+                <p class="period-text"></p>
+            </div>
+            
+            <div class="period">
+                <img src="" alt="" class="period-image">
+                <h3 class="period-title"></h3>
+                <p class="period-text"></p>
+            </div>
+            
+            <div class="period">
+                <img src="" alt="" class="period-image">
+                <h3 class="period-title"></h3>
+                <p class="period-text"></p>
+            </div>
+            
+            <div class="period">
+                <img src="" alt="" class="period-image">
+                <h3 class="period-title"></h3>
+                <p class="period-text"></p>
+            </div>
+            
+            <div class="period">
+                <img src="" alt="" class="period-image">
+                <h3 class="period-title"></h3>
+                <p class="period-text"></p>
+            </div>
+        </div>
+    </main>
+
+</body>
+</html>
