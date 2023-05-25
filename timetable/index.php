c6a3b9d7198c0b087bc44e1bd0a36254b61d1539
Skyman-2 <yannis3736@gmail.com>
2023-05-25 14:45:54 +0200

    HTML of the 1st of June done

--- timetable/index.php
+++ timetable/index.php
@@ -16,40 +16,81 @@
 
     <main>
         <div class="main-wrapper">
+
+            <div class="new-day"><h2>1er Juin</h2></div>
+
+            <div class="period">
+                <img src="" alt="" class="period-image">
+                <h3 class="period-title">Match de Football</h3>
+                <p class="period-text"><span class="period-time">13h-17h</span>Match de football, organisé par <i>Gael, Kaïs, et Mr. Michalet</i>. L'évènement est accompagné d'une buvette.</p>
+            </div>
+
+            <div class="period major-period">
+                <img src="" alt="" class="period-image">
+                <h3 class="period-title">Lancement officiel de <b>La Ronde de l'Espoir</b></h3>
+                <p class="period-text"><span class="period-time">17h</span>Lancement officiel de La Ronde de l'Espoir. Réunion préliminaire pour les participants du record du monde. Il y a aussi une buvette tenue par le <i>CVL</i>.</p>
+            </div>
+            
+            <div class="period">
+                <img src="" alt="" class="period-image">
+                <h3 class="period-title">Pièce de théâtre</h3>
+                <p class="period-text"><span class="period-time">18h-19h</span>Représentation des Terminales Spécialités : Extraits du Soulier de Satin de Claudel et de Richard III de Shakespeare.</p>
+            </div>
+            
+            <div class="period important-period">
+                <img src="" alt="" class="period-image">
+                <h3 class="period-title">Départ des avirons</h3>
+                <p class="period-text"><span class="period-time">19h</span>Départ du record d'ergo aviron dans le hall.</p>
+            </div>
+            
+            <div class="period">
+                <img src="" alt="" class="period-image">
+                <h3 class="period-title">Buvette</h3>
+                <p class="period-text"><span class="period-time">19h-23h</span>Buvette et repas pour les rameurs principalement, tenue par le <i>CVL</i>.</p>
+            </div>
+
+            <div class="period danger-period">
+                <img src="" alt="" class="period-image">
+                <h3 class="period-title">Fin de la journée</h3>
+                <p class="period-text"><span class="period-time">21h</span>Fermeture du lycée pour tout le monde excepté le staff pour l'organisation du record et les rameurs.</p>
+            </div>
+
+            <div class="new-day"><h2>2 Juin</h2></div>
+            
             <div class="period">
                 <img src="" alt="" class="period-image">
                 <h3 class="period-title"></h3>
-                <p class="period-text"></p>
+                <p class="period-text"><span class="period-time"></span></p>
             </div>
 
             <div class="period">
                 <img src="" alt="" class="period-image">
                 <h3 class="period-title"></h3>
-                <p class="period-text"></p>
+                <p class="period-text"><span class="period-time"></span></p>
             </div>
             
             <div class="period">
                 <img src="" alt="" class="period-image">
                 <h3 class="period-title"></h3>
-                <p class="period-text"></p>
+                <p class="period-text"><span class="period-time"></span></p>
             </div>
             
             <div class="period">
                 <img src="" alt="" class="period-image">
                 <h3 class="period-title"></h3>
-                <p class="period-text"></p>
+                <p class="period-text"><span class="period-time"></span></p>
             </div>
             
             <div class="period">
                 <img src="" alt="" class="period-image">
                 <h3 class="period-title"></h3>
-                <p class="period-text"></p>
+                <p class="period-text"><span class="period-time"></span></p>
             </div>
             
             <div class="period">
                 <img src="" alt="" class="period-image">
                 <h3 class="period-title"></h3>
-                <p class="period-text"></p>
+                <p class="period-text"><span class="period-time"></span></p>
             </div>
         </div>
     </main>
